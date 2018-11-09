2aebaa2ce74e0129f5192e4b84f07fd50c37dc11
siddharth <pscrusher101@gmail.com>
2018-11-09 16:36:04 +0100

    js files

---
+++ js/popup.js
@@ -0,0 +1,49 @@
+// Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
+// Get the modal for user
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
+// Get the modal for member
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
